f4baccdfd1613be21a049566cc769a375133449a
nicolasraffray <nraffray@gmail.com>
2020-03-28 23:59:03 +0000

    can get lat and lng coordinates from user click

--- covid_mapper/src/components/map.jsx
+++ covid_mapper/src/components/map.jsx
@@ -2,11 +2,46 @@ import React, { Component } from "react";
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapContainer extends Component {
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      lat: null, 
+      lng: null,
+      data: null
+    }
+  }
+
+  onMapClicked = (mapProps, map, event) => {
+    const lat = event.latLng.lat()
+    const lng = event.latLng.lng()
+    console.log(lat, lng)
+
+    // google
+
+
+    // let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyA6sCpEaxSZBY0P5v7ugZZ2HgJlAdlyhiQ`
+    // fetch(url)
+    //   .then(response => 
+    //     this.setState(
+    //       {
+    //         data: response
+    //       }
+    //     ))
+  }
+
   render() {
+    console.log(this.state.data)
     return (
-      <Map google={this.props.google} zoom={14}>
- 
-        
+  
+      <Map 
+        google={this.props.google} 
+        initialCenter={{
+          lat: 51.507351,
+          lng: -0.127758
+        }}
+        zoom={2}
+        onClick={this.onMapClicked}> 
       </Map>
     );
   }
