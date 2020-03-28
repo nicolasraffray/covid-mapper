16d34f7ff722afaf63bdba3f2ef2e8bf050d43be
nicolasraffray <nraffray@gmail.com>
2020-03-28 22:41:17 +0000

    map replaced with google api map

--- covid_mapper/src/components/map.jsx
+++ covid_mapper/src/components/map.jsx
@@ -1,19 +1,17 @@
 import React, { Component } from "react";
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-class Map extends Component {
+export class MapContainer extends Component {
   render() {
     return (
-      <div className="map">
-        <h4>Map</h4>
-        <img
-          className="mapImage"
-          height="500px"
-          width="750px"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/World_map_%28Miller_cylindrical_projection%2C_blank%29.svg/1280px-World_map_%28Miller_cylindrical_projection%2C_blank%29.svg.png"
-        ></img>
-      </div>
+      <Map google={this.props.google} zoom={14}>
+ 
+        
+      </Map>
     );
   }
 }
-
-export default Map;
+ 
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyA6sCpEaxSZBY0P5v7ugZZ2HgJlAdlyhiQ")
+})(MapContainer)
