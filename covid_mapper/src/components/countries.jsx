9f90432dbd4536e8479b850efb659092dba7ca0f
Asia <asiaellis5@gmail.com>
2020-03-29 18:30:34 +0100

    mapped over America to get totals from all states

--- covid_mapper/src/components/countries.jsx
+++ covid_mapper/src/components/countries.jsx
@@ -4,6 +4,7 @@ import Country from "./country.jsx";
 class Countries extends Component {
   state = {
     countries: [],
+    US: [],
     error: null,
     isLoaded: false
   };
@@ -24,6 +25,7 @@ class Countries extends Component {
         this.setState(
           {
             countries: response.data["covid19Stats"].slice(3257, 3430),
+            US: this.updateUS(response.data["covid19Stats"].slice(0, 3170)),
             isLoaded: true,
             error: false
           },
@@ -37,15 +39,42 @@ class Countries extends Component {
       );
   }
 
+  updateUS(USA) {
+    const total = [0,0,0]
+    USA.map(state => (
+      total[0] += state.confirmed
+    ))
+    USA.map(state => (
+      total[1] += state.recovered
+    ))
+    USA.map(state => (
+      total[2] += state.deaths
+    ))
+    return ([{"country": "US", 
+            "confirmed": total[0],
+            "recovered": total[1],
+            "deaths": total[2]}])
+  }
+
 
   render() {
     
     console.log(this.state.countries)
+    console.log(this.state.US)
   
     return (
       <div className="countries">
         <h4>Country Statistics</h4>
         {this.state.countries.map(country => (
+          <Country
+            key={country.country}
+            name={country.country}
+            infections={country.confirmed}
+            deaths={country.deaths}
+            recoveries={country.recovered}
+          />
+        ))}
+        {this.state.US.map(country => (
           <Country
             key={country.country}
             name={country.country}
