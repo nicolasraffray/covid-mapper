41743709e4fb6c290c3f9c4e630aecea7c760bce
David Papamichael <davidpapamichael@gmail.com>
2020-04-01 15:26:50 +0100

    Info Windows added to makers on map

--- covid_mapper/src/components/map.jsx
+++ covid_mapper/src/components/map.jsx
@@ -1,34 +1,56 @@
 import React, { Component } from "react";
-import { Map, GoogleApiWrapper, Marker, Circle } from 'google-maps-react';
-import styles from './assets/mapStyle.json'
-
+import {
+  Map,
+  GoogleApiWrapper,
+  Marker,
+  InfoWindow,
+  Circle
+} from "google-maps-react";
+import styles from "./assets/mapStyle.json";
 
 export class MapContainer extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
+      isMarkerShowing: true,
+      activeMarker: null,
       lat: null,
       lng: null,
-      data: null
+      data: null,
+      selectedPlace: {},
+      activeMarker: {}
     };
   }
 
   onMapClicked = (mapProps, map, event) => {
-    console.log("YOU CLICKED ON THE MAP")
-    const lat = event.latLng.lat();
-    const lng = event.latLng.lng();
-    // console.log("lat long",lat, lng)
-  }
+    console.log("YOU CLICKED ON THE MAP");
 
-  onMarkerClicked = (mapProps, marker, event) => {
-    const lat = event.latLng.lat();
-    const lng = event.latLng.lng();
-    console.log("You Clicked on a Marker")
-    // console.log("lat long",lat, lng)
+    this.setState({
+      isMarkerShowing: false,
+      activeMarker: ""
+    });
+    console.log("mapClicked", this.state.isMarkerShowing);
+  };
+
+  onMarkerClicked = (props, marker, event) => {
+    console.log("THIS IS THE MARKER ID", marker.id);
+
+    this.setState({
+      isMarkerShowing: true,
+      selectedPlace: props,
+      activeMarker: marker
+      // activeMarker: marker.id
+    });
+
+    console.log("isMarkerShowing", this.state.isMarkerShowing);
+    // console.log("You Clicked on a Marker");
+    // console.log("MapProps", mapProps);
+    // console.log("marker", marker);
+    // console.log("event", event);
+    // console.log("lat long", lat, lng);
     // console.log("Props", mapProps);
-    // console.log("Map", marker)
-    // console.log("event", event)
+    console.log("Map", marker);
+    console.log("event", event);
   };
 
   apiIsLoaded = (map, maps, center) => {
@@ -41,38 +63,44 @@ export class MapContainer extends Component {
       map,
       center: center,
       radius: 60000
-      
     });
   };
 
   generateMarkers = () => {
-    return this.props.countries.map((country, index) => {
-      return <Marker key={index} 
-                     id={country.country} 
-                     position={ country.center}
-                     onClick={this.onMarkerClicked}
-                     onMouseOver={this.onMarkerClicked}
-                     icon={"http://maps.google.com/mapfiles/ms/icons/blue.png"}/>
-    }
-  )}
-
+    return this.props.countries.map((country, i) => {
+      return (
+        <Marker
+          id={country.country}
+          key={i}
+          position={country.center}
+          onClick={this.onMarkerClicked}
+        ></Marker>
+      );
+    });
+  };
 
   render() {
     // console.log(styles)
     return (
       <Map
-      google={this.props.google}
-      styles={styles}
-      initialCenter={{ lat: 29.94, lng: 31.548}}
-      zoom={2.25}
-      minZoom={2.24}
-      maxZoom={12}
-      onClick={this.onMapClicked}
-      
-    
-    >
-      {this.generateMarkers()}
-    </Map>
+        google={this.props.google}
+        styles={styles}
+        initialCenter={{ lat: 29.94, lng: 31.548 }}
+        zoom={2.25}
+        minZoom={2.24}
+        maxZoom={12}
+        onClick={this.onMapClicked}
+      >
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.isMarkerShowing}
+        >
+          <div>
+            <h1>{this.state.selectedPlace.name}</h1>
+          </div>
+        </InfoWindow>
+        {this.generateMarkers()}
+      </Map>
     );
     // };
   }
